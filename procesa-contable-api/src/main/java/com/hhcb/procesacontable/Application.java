a1e074804b6c2f237eb3f645773dc4c9bf6302e8
helmerhdez <helmer0013@gmail.com>
2024-03-25 09:50:02 -0500

    Estructura inicial SpringBoot

--- procesa-contable-api/src/main/java/com/hhcb/procesacontable/Application.java
+++ procesa-contable-api/src/main/java/com/hhcb/procesacontable/Application.java
@@ -1,7 +1,6 @@
 package com.hhcb.procesacontable;
 
 import com.hhcb.procesacontable.config.SpringConfig;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
